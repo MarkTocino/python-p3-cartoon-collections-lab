7379c92d3b87ad89b33dd4ee214855927c060ded
MarkTocino <Mark_tocino@yahoo.com>
2023-07-25 19:31:46 -0400

    Completed Lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
+def roll_call_dwarves(names):
+    for planeteers in names:
+        print(f"{names.index(planeteers) + 1}. {planeteers}")
     pass
 
-def summon_captain_planet():
+def summon_captain_planet(elements):
+    return [names.capitalize() + '!' for names in elements]
+    pass
+# call is what is in the array, each on of them
+def long_planeteer_calls(calls):
+    print(calls)
+    for call in calls:
+        if len(calls) > 4:
+            return False
+        else: 
+            return True
     pass
 
-def long_planeteer_calls():
-    pass
-
-def find_the_cheese():
+def find_the_cheese(type):
+    for taste in type:
+        if taste == "cheddar" or taste == "gouda" or taste == "camembert":
+            return taste
     pass
